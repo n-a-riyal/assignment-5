aa9df4c0d6ea9ac314e1e1afd8ec3650ba5fe5dc
Nure Alam Riyal <20223656640alam@juniv.edu>
2024-09-23 22:10:01 +0600

    same javascript

--- js/index.js
+++ js/index.js
@@ -1,6 +1,30 @@
 document.getElementById('btn1').addEventListener('click',function(){
-//  const donate1=document.getElementById('donate1');
-//  const addMoney1=document.getElementById('addMoney1');
+
     calculateDonate('donate1', 'addMoney1')
-    // console.log(donate1, addMoney1)
+    addhistory('donate1','noakhali')
+    
+})
+document.getElementById('btn2').addEventListener('click',function(){
+
+    calculateDonate('donate2', 'addMoney2')
+    addhistory('donate2','feni')
+    
+})
+document.getElementById('btn3').addEventListener('click',function(){
+
+    calculateDonate('donate3', 'addMoney3')
+    addhistory('donate3','movment')
+    
+})
+document.getElementById('title-donate').addEventListener('click',function(){
+
+styleChangeOfHistoryAndDonate('title-donate')
+document.getElementById('historyPage').classList.add('hidden')
+document.getElementById('donatePage').classList.remove('hidden')
+
+})
+document.getElementById('title-history').addEventListener('click',function(){
+styleChangeOfHistoryAndDonate('title-history')
+document.getElementById('donatePage').classList.add('hidden')
+document.getElementById('historyPage').classList.remove('hidden')
 })
--- js/utilites.js
+++ js/utilites.js
@@ -1,6 +1,13 @@
+// history and donate 
+function styleChangeOfHistoryAndDonate(id){
+  document.getElementById('title-donate').classList.remove('bg-[#B4F461]','text-xl')
+  document.getElementById('title-history').classList.remove('bg-[#B4F461]','text-xl')
+  document.getElementById(id).classList.add('bg-[#B4F461]','text-xl')
+
+}
 
 const mainBlance=document.getElementById('myBlance')
-
+const historyDiv=document.getElementById('historyPage')
 function backTodigint(id){
     const number1=document.getElementById(id).value;
  return parseFloat(number1);
@@ -13,4 +20,23 @@ function calculateDonate(id1,id2){
    const addMoney= document.getElementById(id2);
    addMoney.innerText=backTodigint(id1)+backTodigintFormString(id2);
   mainBlance.innerText=backTodigintFormString('myBlance')-backTodigint(id1);
+}
+function addhistory(donate,place){
+
+   const div=document.createElement('div');
+   
+   const div1=document.createElement('div');
+   div1.innerHTML=`<p>${backTodigint(donate)}  ${document.getElementById(place).innerText} </p>
+   
+   <h5>${new Date()}</h5>
+   
+   `
+   div1.classList.add('p-3', 'border','space-y-2','rounded-xl')
+   div.appendChild(div1)
+   
+   historyDiv.appendChild(div)
+  
+
+
+
 }
