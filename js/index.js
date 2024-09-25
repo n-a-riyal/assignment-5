a9e312cfccc3c0126d2b61b29faa584ef3288637
riyal <20223656640alam@juniv.edu>
2024-09-25 06:31:15 +0600

    i think every things ok

--- js/index.js
+++ js/index.js
@@ -1,64 +1,67 @@
-document.getElementById('btn1').addEventListener('click',function(){
-          validityTest('donate1','donate1');
-          
-                calculateDonate('donate1', 'addMoney1','noakhali','modal1')
-            
-        
-   document.getElementById('donate1').value="";
+const blogs1 = document.getElementById("blogs")
+
+
+
+
+
+document.getElementById('btn1').addEventListener('click', function () {
+    validityTest('donate1', 'donate1');
+    calculateDonate('donate1', 'addMoney1', 'noakhali', 'modal1')
+    document.getElementById('donate1').value = "";
 
 })
-document.getElementById('btn2').addEventListener('click',function(){
-    validityTest('donate2','donate2');
-    calculateDonate('donate2', 'addMoney2','feni','modal2')
-    
-   
-
-    document.getElementById('donate2').value="";
+document.getElementById('btn2').addEventListener('click', function () {
+    validityTest('donate2', 'donate2');
+    calculateDonate('donate2', 'addMoney2', 'feni', 'modal2')
+    document.getElementById('donate2').value = "";
 })
-document.getElementById('btn3').addEventListener('click',function(){
-    validityTest('donate3','donate3');
-    calculateDonate('donate3', 'addMoney3','movment','modal3')
-   
-    document.getElementById('donate3').value="";
-    
-})
-document.getElementById('title-donate').addEventListener('click',function(){
+document.getElementById('btn3').addEventListener('click', function () {
+    validityTest('donate3', 'donate3');
+    calculateDonate('donate3', 'addMoney3', 'movment', 'modal3')
 
-styleChangeOfHistoryAndDonate('title-donate')
-document.getElementById('historyPage').classList.add('hidden')
-document.getElementById('donatePage').classList.remove('hidden')
-document.getElementById('title-history').classList.add('border')
+    document.getElementById('donate3').value = "";
 
 })
-document.getElementById('title-history').addEventListener('click',function(){
-styleChangeOfHistoryAndDonate('title-history')
-document.getElementById('donatePage').classList.add('hidden')
-document.getElementById('historyPage').classList.remove('hidden')
-document.getElementById('title-donate').classList.add('border')
+
+
+document.getElementById('title-donate').addEventListener('click', function () {
+    styleChangeOfHistoryAndDonate('title-donate')
+    document.getElementById('historyPage').classList.add('hidden')
+    document.getElementById('donatePage').classList.remove('hidden')
+    document.getElementById('title-history').classList.add('border')
+
 })
 
-const blogs1=document.getElementById("blogs")
 
-blogs1.addEventListener('click',function(){
-   
- 
+document.getElementById('title-history').addEventListener('click', function () {
+    styleChangeOfHistoryAndDonate('title-history')
+    document.getElementById('donatePage').classList.add('hidden')
+    document.getElementById('historyPage').classList.remove('hidden')
+    document.getElementById('title-donate').classList.add('border')
+})
 
-    const link1=document.getElementById('link1')
-    link1.setAttribute('href','./blog.html')
-    
-       
-    })
-   
-document.getElementById('dmodal1').addEventListener('click',function(){
+
+
+
+blogs1.addEventListener('click', function () {
+    const link1 = document.getElementById('link1')
+    link1.setAttribute('href', './blog.html')
+
+
+})
+
+
+
+document.getElementById('dmodal1').addEventListener('click', function () {
     document.getElementById('modal1').classList.add('hidden');
 })
-document.getElementById('dmodal2').addEventListener('click',function(){
+document.getElementById('dmodal2').addEventListener('click', function () {
     document.getElementById('modal2').classList.add('hidden');
 })
-document.getElementById('dmodal3').addEventListener('click',function(){
+document.getElementById('dmodal3').addEventListener('click', function () {
     document.getElementById('modal3').classList.add('hidden');
 })
-   
-    
-   
+
+
+
 
--- js/utilites.js
+++ js/utilites.js
@@ -1,65 +1,81 @@
+const mainBlance = document.getElementById('myBlance')
+const historyDiv = document.getElementById('historyPage')
+
+
+
 // history and donate 
-function styleChangeOfHistoryAndDonate(id){
-  document.getElementById('title-donate').classList.remove('bg-[#B4F461]','text-xl')
-  document.getElementById('title-history').classList.remove('bg-[#B4F461]','text-xl')
-  document.getElementById(id).classList.add('bg-[#B4F461]','text-xl')
+function styleChangeOfHistoryAndDonate(id) {
+  document.getElementById('title-donate').classList.remove('bg-[#B4F461]', 'text-xl')
+  document.getElementById('title-history').classList.remove('bg-[#B4F461]', 'text-xl')
+  document.getElementById(id).classList.add('bg-[#B4F461]', 'text-xl')
 
 }
-function validityTest(id1,id2){
-  const value1=backTodigint(id1);
- const value2=document.getElementById(id2)
-  if(value1<0 || isNaN(value1)){
-    document.getElementById(id1).value="";
+
+
+
+function validityTest(id1, id2) {
+  const value1 = backTodigint(id1);
+  const value2 = document.getElementById(id2)
+  if (value1 < 0 || isNaN(value1)) {
+    document.getElementById(id1).value = "";
     alert('INVALID NUMBER')
     return trure;
   }
- 
+
 
 }
 
-const mainBlance=document.getElementById('myBlance')
-const historyDiv=document.getElementById('historyPage')
-function backTodigint(id){
-    const number1=document.getElementById(id).value;
- return parseFloat(number1);
+
+function backTodigint(id) {
+  const number1 = document.getElementById(id).value;
+  return parseFloat(number1);
 }
-function backTodigintFormString(id){
-   const number1=document.getElementById(id).innerText;
-return parseFloat(number1);
+
+
+
+function backTodigintFormString(id) {
+  const number1 = document.getElementById(id).innerText;
+  return parseFloat(number1);
 }
-function calculateDonate(id1,id2,id3,id4){
-   const addMoney= document.getElementById(id2);
-   const newMyBalence=backTodigintFormString('myBlance')-backTodigint(id1);
-   console.log(backTodigintFormString('myBlance'));
-   if((newMyBalence)<0 ){
+
+
+
+
+function calculateDonate(id1, id2, id3, id4) {
+  const addMoney = document.getElementById(id2);
+  const newMyBalence = backTodigintFormString('myBlance') - backTodigint(id1);
+  console.log(backTodigintFormString('myBlance'));
+  if ((newMyBalence) < 0) {
     alert("Insufficient Blance")
-    document.getElementById(id1).value="";
+    document.getElementById(id1).value = "";
     return;
-   }
-    else{
-      mainBlance.innerText=backTodigintFormString('myBlance')-backTodigint(id1);
-      addMoney.innerText=backTodigint(id1)+backTodigintFormString(id2);
-      addhistory(id1,id3)
-        
-          document.getElementById(id4).classList.remove('hidden')
-    }
-}
-function addhistory(donate,place){
+  }
+  else {
+    mainBlance.innerText = backTodigintFormString('myBlance') - backTodigint(id1);
+    addMoney.innerText = backTodigint(id1) + backTodigintFormString(id2);
+    addhistory(id1, id3)
 
-   const div=document.createElement('div');
-   
-   const div1=document.createElement('div');
-   div1.innerHTML=`<p>${backTodigint(donate)}  ${document.getElementById(place).innerText} </p>
+    document.getElementById(id4).classList.remove('hidden')
+  }
+}
+
+
+
+
+function addhistory(donate, place) {
+  const div = document.createElement('div');
+  const div1 = document.createElement('div');
+  div1.innerHTML = `<p>${backTodigint(donate)} Taka is donated  ${document.getElementById(place).innerText} </p>
    
    <h5>${new Date()}</h5>
    
    `
-   div1.classList.add('p-3', 'border','space-y-2','rounded-xl')
-   div.appendChild(div1)
-   
-   historyDiv.appendChild(div)
-  
+  div1.classList.add('p-3', 'border', 'space-y-2', 'rounded-xl')
+  div.appendChild(div1)
 
+  historyDiv.appendChild(div)
+div1.firstChild.classList.add('font-bold','text-xl')
+div1.lastChild.classList.add('text-sm')
 
 
 }
